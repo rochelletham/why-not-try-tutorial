048bbbf029193dd5925b1b2a7c872fd86271bf1b
rochelletham <rochelle.tham@gmail.com>
2025-11-02 23:07:09 -0800

    Initial Commit

---
+++ Singing_Stella/Singing_StellaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Singing_StellaApp.swift
+//  Singing_Stella
+//
+//  Created by Rochelle Tham on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Singing_StellaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
